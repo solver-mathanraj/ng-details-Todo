5e1751064138fa689ec01933369271e0b5178734
solver-mathanraj <matheesh1214@gmail.com>
2025-04-03 16:44:42 +0530

    router correction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TableComponent } from './table/table.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [TableComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
